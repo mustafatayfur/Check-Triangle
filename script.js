34676d804089080ef0dfe1005c5dd74e2b919d09
mustafa <mltm0204@gmail.com>
2021-11-10 23:15:38 +0300

    images are added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const secondSide = document.querySelector("#input2");
 const thirdSide = document.querySelector("#input3");
 const checkButton = document.querySelector("#button-addon2");
 const result = document.querySelector("#result");
-const img_result = document.querySelector("#footer");
+const img_result = document.querySelector("#result-img");
 
 
 
@@ -21,15 +21,16 @@ checkButton.addEventListener('click', ()=>{
                 if(side1==side2 && side1==side3 && side2==side3){
 
                     result.innerHTML = 'Equilateral';
+                    img_result.src = "https://i.ya-webdesign.com/images/equilateral-triangle-png-1.png";
                 
                 }else if(side1==side2 || side1==side3 || side2==side3){
                     
                     result.innerHTML = 'Isosceles';
+                    img_result.src = "https://www.mathword.co/wp-content/uploads/2018/06/isosceles-triangle.png";
 
                 }else{
                     result.innerHTML = 'Scalene';
-                    img_result.innerHTML = "<img src=\"https://www.seekpng.com/png/detail/104-1047876_scalene-isosceles-triangle-png.png\"></img>";
-                   
+                    img_result.src = "https://www.seekpng.com/png/detail/104-1047876_scalene-isosceles-triangle-png.png";
                 }
 
         }else{
@@ -37,7 +38,7 @@ checkButton.addEventListener('click', ()=>{
 
         };
     }else{
-        result.innerHTML = 'Invalid Triangle.';
+        result.innerHTML = 'Invalid Triangle. ';
     };
 
 });
