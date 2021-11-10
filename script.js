5b67ed2bc6adf789ce10e783850df9fa3d2c3ace
mustafa <mltm0204@gmail.com>
2021-11-11 00:07:56 +0300

    js codes are added

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const firstSide = document.querySelector("#input1");
 const secondSide = document.querySelector("#input2");
 const thirdSide = document.querySelector("#input3");
 const checkButton = document.querySelector("#button-addon2");
-const result = document.querySelector("#result");
 const img_result = document.querySelector("#result-img");
-
-
 
 checkButton.addEventListener('click', ()=>{
     side1 = +(firstSide.value);
@@ -20,25 +17,52 @@ checkButton.addEventListener('click', ()=>{
 
                 if(side1==side2 && side1==side3 && side2==side3){
 
-                    result.innerHTML = 'Equilateral';
+                    const pEl1 = document.createElement("p");
+                    const footerDiv = document.getElementById("footer");
+                    footerDiv.appendChild(pEl1);
+                    pEl1.setAttribute('id','result1');
+                    const result1 = document.querySelector("#result1");
+                    result1.innerHTML = 'Your triangle is Equilateral';
                     img_result.src = "https://i.ya-webdesign.com/images/equilateral-triangle-png-1.png";
                 
                 }else if(side1==side2 || side1==side3 || side2==side3){
                     
-                    result.innerHTML = 'Isosceles';
+                    const pEl1 = document.createElement("p");
+                    const footerDiv = document.getElementById("footer");
+                    footerDiv.appendChild(pEl1);
+                    pEl1.setAttribute('id','result1');
+                    const result1 = document.querySelector("#result1");
+                    result1.innerHTML = 'Your triangle is Isosceles';
                     img_result.src = "https://www.mathword.co/wp-content/uploads/2018/06/isosceles-triangle.png";
 
                 }else{
-                    result.innerHTML = 'Scalene';
+
+                    const pEl1 = document.createElement("p");
+                    const footerDiv = document.getElementById("footer");
+                    footerDiv.appendChild(pEl1);
+                    pEl1.setAttribute('id','result1');
+                    const result1 = document.querySelector("#result1");
+                    result1.innerHTML = 'Your triangle is Scalene';
                     img_result.src = "https://www.seekpng.com/png/detail/104-1047876_scalene-isosceles-triangle-png.png";
+                    
                 }
 
         }else{
-            result.innerHTML = 'Invalid Triangle.';
+            const pEl2 = document.createElement("p");
+            const footerDiv = document.getElementById("footer");
+            footerDiv.appendChild(pEl2);
+            pEl2.setAttribute('id','result2');
+            const result2 = document.querySelector("#result2");
+            result2.innerHTML = 'Invalid Triangle. The sum of the lengths of any two sides must be greater than the third side';
 
         };
     }else{
-        result.innerHTML = 'Invalid Triangle. ';
+        const pEl2 = document.createElement("p");
+        const footerDiv = document.getElementById("footer");
+        footerDiv.appendChild(pEl2);
+        pEl2.setAttribute('id','result2');
+        const result2 = document.querySelector("#result2");
+        result2.innerHTML = 'Invalid Triangle. All sides have to be of length > 0';
     };
 
 });
